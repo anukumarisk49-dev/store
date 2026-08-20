3a045aa428d05d9becc6e438d635e1a777bb44c6
anukumarisk49-dev <anukumarisk49@gmail.com>
2026-08-20 15:57:38 +0530

    added new url

--- src/data/affiliate/beauty.ts
+++ src/data/affiliate/beauty.ts
@@ -1,5 +1,5 @@
 export const beautyAffiliateLinks: Record<string, string> = {
-  'beauty-1': 'https://www.nykaa.com/search/result/?q=skincare',
+  'beauty-1': 'https://fktr.in/H31q22U',
   'beauty-2': 'https://www.nykaa.com/search/result/?q=makeup',
   'beauty-3': 'https://www.amazon.in/s?k=floral+perfume',
 };
